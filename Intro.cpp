d06932cd56c9fee70ba44126c3cf73484b029d37
ThienNguyen15 <144821367+ThienNguyen15@users.noreply.github.com>
2023-10-13 14:25:01 +0700

    Intro.cpp

--- Intro.cpp
+++ Intro.cpp
@@ -1,7 +1,7 @@
 	Assignment1 - RESTAURANT OPERATIONS (Part1)
-		• Implement circular linked list data structure.
-		• Implement basic operations on circular linked list data type.
+		â€¢ Implement circular linked list data structure.
+		â€¢ Implement basic operations on circular linked list data type.
 
-	Assignment2 - RESTAURANT OPERATIONS(Part2)
-		• Implement a tree data structure and become familiar with hashtables.
-		• Select and apply appropriate data structures to achieve desired results.
+	Assignment2 - RESTAURANT OPERATIONS (Part2)
+		â€¢ Implement a tree data structure and become familiar with hashtables.
+		â€¢ Select and apply appropriate data structures to achieve desired results.
